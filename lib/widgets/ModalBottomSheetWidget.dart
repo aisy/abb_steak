ec5781116c959d2314c5b39324d9445e438a40b5
aisy <aisymuhammad@gmail.com>
2021-06-13 12:27:47 +0700

    add class ModalBottomSheet widget to manage Modal

---
+++ lib/widgets/ModalBottomSheetWidget.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+class ModalBottomSheetWidget {
+  // ScrollController _controller = ScrollController();
+
+  void modalBottomSheetMenu(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        backgroundColor: Colors.transparent,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(20),
+          ),
+        ),
+        builder: (context) {
+          return DraggableScrollableSheet(
+              initialChildSize: 0.8,
+              minChildSize: 0.5,
+              maxChildSize: 0.8,
+              builder: (_, controller) {
+                return Container(
+                  padding: EdgeInsets.only(
+                      right: 20.0, left: 20.0, bottom: 20.0, top: 10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                  ),
+                  child: ListView(
+                    controller: controller,
+                    children: <Widget>[
+                      Center(
+                        child: Container(
+                          child: null,
+                          height: 4,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 20.0),
+                        height: 250,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  'https://googleflutter.com/sample_image.jpg'),
+                              fit: BoxFit.cover),
+                        ),
+                        child: null,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 20, bottom: 10),
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          "Judul Konten",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700, fontSize: 20),
+                        ),
+                      ),
+                      Text(
+                          "Lorem enim aliquip veniam occaecat enim cillum quis laboris sunt aute consectetur do. Id sint do consectetur incididunt nostrud in laborum sunt pariatur mollit velit nisi aute. "),
+                    ],
+                  ),
+                );
+              });
+        });
+  }
+
+  void modalBottomSheetOrder(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        backgroundColor: Colors.transparent,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(20),
+          ),
+        ),
+        builder: (context) {
+          return DraggableScrollableSheet(
+              initialChildSize: 0.8,
+              minChildSize: 0.5,
+              maxChildSize: 0.8,
+              builder: (_, controller) {
+                return Container(
+                  padding: EdgeInsets.only(
+                      right: 20.0, left: 20.0, bottom: 20.0, top: 10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                  ),
+                  child: ListView(
+                    controller: controller,
+                    children: <Widget>[
+                      Center(
+                        child: Container(
+                          child: null,
+                          height: 4,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              });
+        });
+  }
+}
