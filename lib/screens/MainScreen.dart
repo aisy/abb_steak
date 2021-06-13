2dd995a1df8f91f83a75e28a5ce1f0bba92a7673
aisy <aisymuhammad@gmail.com>
2021-06-13 12:29:00 +0700

    implement modal from other class

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -1,4 +1,5 @@
 import 'package:abuba_steak_app/widgets/AppbarWidget.dart';
+import 'package:abuba_steak_app/widgets/ModalBottomSheetWidget.dart';
 import 'package:abuba_steak_app/widgets/ResponsiveLayoutWidget.dart';
 import 'package:abuba_steak_app/widgets/common/CardMenuWidget.dart';
 import 'package:abuba_steak_app/widgets/maxWidthContainerWidget.dart';
@@ -14,6 +15,8 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   String menuFilter = "all items";
 
+  ModalBottomSheetWidget modalBottom = new ModalBottomSheetWidget();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,74 +27,6 @@ class _MainScreenState extends State<MainScreen> {
         ),
       ),
     );
-  }
-
-  void modalBottomSheetShow() {
-    showModalBottomSheet(
-        context: context,
-        isScrollControlled: true,
-        backgroundColor: Colors.transparent,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            top: Radius.circular(20),
-          ),
-        ),
-        builder: (context) {
-          return DraggableScrollableSheet(
-              initialChildSize: 0.8,
-              minChildSize: 0.5,
-              maxChildSize: 0.8,
-              builder: (_, controller) {
-                return Container(
-                  padding: EdgeInsets.only(
-                      right: 20.0, left: 20.0, bottom: 20.0, top: 10.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(20),
-                    ),
-                  ),
-                  child: ListView(
-                    controller: controller,
-                    children: <Widget>[
-                      Center(
-                        child: Container(
-                          child: null,
-                          height: 4,
-                          width: 30,
-                          decoration: BoxDecoration(
-                            color: Colors.grey,
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 20.0),
-                        height: 250,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: NetworkImage(
-                                  'https://googleflutter.com/sample_image.jpg'),
-                              fit: BoxFit.cover),
-                        ),
-                        child: null,
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 20, bottom: 10),
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          "Judul Konten",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w700, fontSize: 20),
-                        ),
-                      ),
-                      Text(
-                          "Lorem enim aliquip veniam occaecat enim cillum quis laboris sunt aute consectetur do. Id sint do consectetur incididunt nostrud in laborum sunt pariatur mollit velit nisi aute. "),
-                    ],
-                  ),
-                );
-              });
-        });
   }
 
   // Mobile Layout
@@ -121,9 +56,12 @@ class _MainScreenState extends State<MainScreen> {
                       mainAxisSpacing: 10,
                       children: <Widget>[
                         CardMenuWidget(
-                            title: "Destroy",
-                            img: 'https://googleflutter.com/sample_image.jpg',
-                            onPress: () => modalBottomSheetShow()),
+                          title: "Destroy",
+                          img: 'https://googleflutter.com/sample_image.jpg',
+                          // onPress: () => modalBottomSheetMenu(context),
+                          onPress: () =>
+                              modalBottom.modalBottomSheetMenu(context),
+                        ),
                         // cardMenu(),
                         // cardMenu(),
                         // cardMenu(),
