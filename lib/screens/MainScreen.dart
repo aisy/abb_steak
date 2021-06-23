41e117799d56c826539394716119b86993761e56
aisy <aisymuhammad@gmail.com>
2021-06-23 16:30:02 +0700

    add list category button

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:abuba_steak_app/widgets/AppbarWidget.dart';
+import 'package:abuba_steak_app/widgets/ButtonMenuWidget.dart';
 import 'package:abuba_steak_app/widgets/ModalBottomSheetWidget.dart';
 import 'package:abuba_steak_app/widgets/ResponsiveLayoutWidget.dart';
 import 'package:abuba_steak_app/widgets/common/CardMenuWidget.dart';
@@ -56,7 +57,7 @@ class _MainScreenState extends State<MainScreen> {
   // Mobile Layout
   Widget mobileLayout() {
     return Container(
-      padding: EdgeInsets.all(10.0),
+      // padding: EdgeInsets.all(10.0),
       child: Row(
         children: [
           Expanded(
@@ -66,36 +67,32 @@ class _MainScreenState extends State<MainScreen> {
                 : Container(
                     child: Column(
                       children: [
+                        listCategory(),
                         Container(
-                          alignment: Alignment.topRight,
-                          child: filterMenuOption(),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Flexible(
-                          child: GridView.builder(
-                              gridDelegate:
-                                  SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 2,
-                                mainAxisSpacing: 10,
-                                crossAxisSpacing: 10,
-                              ),
-                              itemCount: menu.length,
-                              itemBuilder: (context, index) {
-                                return CardMenuWidget(
-                                    title: menu[index]["menu_name"],
-                                    img: menu[index]["menu_img"],
-                                    onPress: () {
-                                      setState(() {
-                                        detailMenu = menu[index];
-                                      });
+                          child: Flexible(
+                            child: GridView.builder(
+                                padding: EdgeInsets.symmetric(horizontal: 20),
+                                gridDelegate:
+                                    SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2,
+                                  mainAxisSpacing: 10,
+                                  crossAxisSpacing: 10,
+                                ),
+                                itemCount: menu.length,
+                                itemBuilder: (context, index) {
+                                  return CardMenuWidget(
+                                      title: menu[index]["menu_name"],
+                                      img: menu[index]["menu_img"],
+                                      onPress: () {
+                                        setState(() {
+                                          detailMenu = menu[index];
+                                        });
 
-                                      // print(detailMenu);
-                                      modalBottom.modalBottomSheetMenu(
-                                          context, detailMenu);
-                                    });
-                              }),
+                                        modalBottom.modalBottomSheetMenu(
+                                            context, detailMenu);
+                                      });
+                                }),
+                          ),
                         ),
                       ],
                     ),
@@ -106,38 +103,78 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 
-  // Widget dataMobile(){
-  //   if()
-  // }
+  Widget listCategory() {
+    return Container(
+      height: 30,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          // filterMenuOption(),
+          ButtonMenuWidget(
+            icon: Icons.tune,
+            iconColor: Colors.black,
+            buttonColor: Colors.white,
+            textColor: Colors.black,
+            buttonText: "Filter",
+            onTap: () => {},
+          ),
+          ButtonMenuWidget(
+            buttonColor: Colors.amber[900],
+            buttonText: "Steak",
+            onTap: () => {},
+          ),
+          ButtonMenuWidget(
+            buttonColor: Colors.red[900],
+            buttonText: "Pasta",
+            onTap: () => {},
+          ),
+          ButtonMenuWidget(
+            buttonColor: Colors.amber[400],
+            buttonText: "Appetizer",
+            onTap: () => {},
+          ),
+          ButtonMenuWidget(
+            buttonColor: Colors.red[200],
+            buttonText: "Desert",
+            onTap: () => {},
+          ),
+          ButtonMenuWidget(
+            buttonColor: Colors.lightBlue[400],
+            buttonText: "Drinks",
+            onTap: () => {},
+          ),
+        ],
+      ),
+    );
+  }
 
   Widget filterMenuOption() {
-    return Padding(
-      padding: EdgeInsets.all(1.0),
-      child: Container(
-        height: 30,
-        padding: EdgeInsets.symmetric(horizontal: 10.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30.0),
-          border: Border.all(color: Colors.grey),
-        ),
-        child: DropdownButtonHideUnderline(
-          child: DropdownButton(
-            style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w400),
-            value: menuFilter,
-            items: [
-              DropdownMenuItem(child: Text("All Items"), value: "all items"),
-              DropdownMenuItem(child: Text("Steak"), value: "steak"),
-              DropdownMenuItem(child: Text("Rice"), value: "rice"),
-              DropdownMenuItem(child: Text("Side"), value: "side"),
-              DropdownMenuItem(child: Text("Snack"), value: "snack"),
-              DropdownMenuItem(child: Text("Drinks"), value: "drink"),
-            ],
-            onChanged: (value) {
-              setState(() {
-                menuFilter = value.toString();
-              });
-            },
-          ),
+    return Container(
+      // height: 30,
+      margin: EdgeInsets.only(right: 5),
+      padding: EdgeInsets.symmetric(horizontal: 10.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        border: Border.all(color: Colors.grey),
+      ),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton(
+          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w400),
+          value: menuFilter,
+          items: [
+            DropdownMenuItem(child: Text("All Items"), value: "all items"),
+            DropdownMenuItem(child: Text("Steak"), value: "steak"),
+            DropdownMenuItem(child: Text("Pasta"), value: "pasta"),
+            // DropdownMenuItem(child: Text("Rice"), value: "rice"),
+            // DropdownMenuItem(child: Text("Side"), value: "side"),
+            // DropdownMenuItem(child: Text("Snack"), value: "snack"),
+            // DropdownMenuItem(child: Text("Drinks"), value: "drink"),
+          ],
+          onChanged: (value) {
+            setState(() {
+              menuFilter = value.toString();
+            });
+          },
         ),
       ),
     );
