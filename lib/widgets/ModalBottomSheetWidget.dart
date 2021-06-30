8d87281274b779865dc282e404646ba872f5893a
aisy <aisymuhammad@gmail.com>
2021-06-30 17:49:50 +0700

    change modal bottom (not fixed, but do later bcs move in main screen)

--- lib/widgets/ModalBottomSheetWidget.dart
+++ lib/widgets/ModalBottomSheetWidget.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -17,76 +18,7 @@ class ModalBottomSheetWidget {
           ),
         ),
         builder: (context) {
-          return DraggableScrollableSheet(
-              initialChildSize: 0.8,
-              minChildSize: 0.5,
-              maxChildSize: 0.8,
-              builder: (_, controller) {
-                return Container(
-                  padding: EdgeInsets.only(
-                      right: 20.0, left: 20.0, bottom: 20.0, top: 10.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(20),
-                    ),
-                  ),
-                  child: ListView(
-                    controller: controller,
-                    children: <Widget>[
-                      Center(
-                        child: Container(
-                          child: null,
-                          height: 4,
-                          width: 30,
-                          decoration: BoxDecoration(
-                            color: Colors.grey,
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 20.0),
-                        height: 250,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: NetworkImage(dataMenu["menu_img"]),
-                              fit: BoxFit.cover),
-                        ),
-                        child: null,
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 20, bottom: 10),
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          dataMenu != null
-                              ? dataMenu["menu_name"]
-                              : "Judul Konten",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w700, fontSize: 20),
-                        ),
-                      ),
-                      Text(dataMenu != null
-                          ? dataMenu["description"]
-                          : "description"),
-                      Container(
-                        margin: EdgeInsets.only(top: 10.0),
-                        child: Row(
-                          children: [
-                            Text(
-                              "${idrCurrencyFormat.format(10000)}",
-                              style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 18,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              });
+          return Container();
         });
   }
 
@@ -134,5 +66,13 @@ class ModalBottomSheetWidget {
                 );
               });
         });
+  }
+
+  String convertPrice(price) {
+    // String convertValue = value.toString();
+    var priceDecode = json.decode(price);
+    // print(priceDecode.con);
+
+    return "Text";
   }
 }
