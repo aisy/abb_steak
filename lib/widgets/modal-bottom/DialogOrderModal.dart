cb3ad3d85ba3b07b947733b961f823b49a1ee226
aisy <aisymuhammad@gmail.com>
2021-07-04 21:51:40 +0700

    add consumer to show list order

--- lib/widgets/modal-bottom/DialogOrderModal.dart
+++ lib/widgets/modal-bottom/DialogOrderModal.dart
@@ -1,4 +1,10 @@
+import 'package:abuba_steak_app/models/orderModel.dart';
+import 'package:abuba_steak_app/widgets/common/CardOrderWidget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
+
+final idrCurrencyFormat = new NumberFormat.simpleCurrency(locale: 'id_ID');
 
 class DialogOrderModal extends StatefulWidget {
   DialogOrderModal({Key? key}) : super(key: key);
@@ -12,190 +18,93 @@ class _DialogOrderModalState extends State<DialogOrderModal> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Stack(
-        children: [
-          Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 0.3,
-                    blurRadius: 2,
-                    offset: Offset(0, 3),
-                  )
-                ]),
-                width: double.infinity,
-                height: 56.0,
-                child: Center(
-                  child: Text(
-                    "Pesananku",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 18,
-                    ),
-                  ), // Your desired title
-                ),
-              ),
-              Positioned(
-                left: 0.0,
-                top: 8.0,
-                child: IconButton(
-                  icon: Icon(
-                    Icons.arrow_back,
-                    color: Colors.black,
-                  ), // Your desired icon
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-              ),
-            ],
-          ),
-          Align(
-            alignment: FractionalOffset.topCenter,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                // Container(
-                //   padding: EdgeInsets.symmetric(horizontal: 10),
-                //   margin: EdgeInsets.only(top: 80, bottom: 20),
-                //   child: Row(
-                //     children: [
-                //       Text("total order:"),
-                //     ],
-                //   ),
-                // ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  margin: EdgeInsets.only(top: 70, bottom: 20),
-                  child: Column(
-                    children: [
-                      cardOrderList(),
-                      cardOrderList(),
-                      cardOrderList(),
-                      cardOrderList(),
-                      cardOrderList(),
-                      cardOrderList(),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            height: 50,
-            width: MediaQuery.of(context).size.width,
-            child: Container(
-              decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 10,
-                  blurRadius: 2,
-                  offset: Offset(0, 7),
-                )
-              ]),
-              child: Text("data"),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
+      child: Consumer<OrderModel>(
+        builder: (context, order, child) {
+          var dataOrder = order.orderValue;
 
-Widget cardOrderList() {
-  return Card(
-    child: Container(
-      padding: EdgeInsets.all(10),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            flex: 2,
-            child: Image.network(
-                "https://homepages.cae.wisc.edu/~ece533/images/arctichare.png"),
-          ),
-          Expanded(
-            flex: 5,
-            child: Container(
-              // padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-              padding: EdgeInsets.only(left: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+          return Stack(
+            children: [
+              Stack(
                 children: [
-                  Text(
-                    "Wagyu Sirloin - Prime United State tenderloin",
-                    style: TextStyle(fontWeight: FontWeight.w700),
+                  Container(
+                    decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 0.3,
+                        blurRadius: 2,
+                        offset: Offset(0, 3),
+                      )
+                    ]),
+                    width: double.infinity,
+                    height: 56.0,
+                    child: Center(
+                      child: Text(
+                        "Pesananku",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                        ),
+                      ), // Your desired title
+                    ),
                   ),
-                  Text(
-                    "Rp. 120000",
-                    style: TextStyle(fontWeight: FontWeight.w500),
+                  Positioned(
+                    left: 0.0,
+                    top: 8.0,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.arrow_back,
+                        color: Colors.black,
+                      ), // Your desired icon
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
                   ),
                 ],
               ),
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              // padding: EdgeInsets.symmetric(vertical: 10),
-              alignment: Alignment.topRight,
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ConstrainedBox(
-                      constraints:
-                          BoxConstraints.tightFor(width: 30, height: 30),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: Icon(
-                          Icons.remove,
-                          color: Colors.white,
-                          size: 12,
+              Align(
+                alignment: FractionalOffset.topCenter,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  margin: EdgeInsets.only(top: 70, bottom: 20),
+                  child: dataOrder.isEmpty
+                      ? Text("Anda belum melakukan order")
+                      : Flexible(
+                          child: ListView.builder(
+                            itemCount: dataOrder.length,
+                            itemBuilder: (context, index) {
+                              return CardOrderWidget(
+                                img: dataOrder[index]["menu_img"],
+                                title: dataOrder[index]["menu_name"],
+                                qty: dataOrder[index]["qty"],
+                                option: dataOrder[index]["option"],
+                                price: dataOrder[index]["price"],
+                              );
+                            },
+                          ),
                         ),
-                        style: ElevatedButton.styleFrom(
-                          elevation: 0,
-                          primary: Colors.green, // <-- Button color
-                        ),
-                      ),
-                    ),
-                    Container(
-                      width: 40,
-                      child: Text(
-                        "1",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontSize: 20,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    ConstrainedBox(
-                      constraints:
-                          BoxConstraints.tightFor(width: 30, height: 30),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: Icon(
-                          Icons.add,
-                          color: Colors.white,
-                          size: 12,
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          elevation: 0,
-                          primary: Colors.green, // <-- Button color
-                        ),
-                      ),
-                    ),
-                  ],
                 ),
               ),
-            ),
-          ),
-        ],
+              Positioned(
+                bottom: 0,
+                height: 50,
+                width: MediaQuery.of(context).size.width,
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 10,
+                      blurRadius: 2,
+                      offset: Offset(0, 7),
+                    )
+                  ]),
+                  child: Text("data"),
+                ),
+              )
+            ],
+          );
+        },
       ),
-    ),
-  );
+    );
+  }
 }
