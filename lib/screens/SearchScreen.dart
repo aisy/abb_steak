c48bfc44df0fed86fa33adb6f3ff9efdc34ff015
aisy <aisymuhammad@gmail.com>
2021-07-13 12:16:37 +0700

    make simple searching with json

--- lib/screens/SearchScreen.dart
+++ lib/screens/SearchScreen.dart
@@ -1,6 +1,7 @@
-import 'package:abuba_steak_app/widgets/ButtonCategoryWidget.dart';
+import 'dart:convert';
 import 'package:abuba_steak_app/widgets/common/OptionCategoryWidget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class SearchScreen extends StatefulWidget {
   SearchScreen({Key? key}) : super(key: key);
@@ -10,13 +11,49 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  String indexSearch = "";
   String nameOption = "";
+  bool loading = false;
+  List menu = [];
+
   List dataCategory = [
     {"id": 1, "name": "beef steak", "value": "beef steak"},
     {"id": 2, "name": "fish steak", "value": "fish steak"},
     {"id": 3, "name": "poultry steak", "value": "poultry steak"},
     {"id": 4, "name": "pasta", "value": "pasta"},
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    loadDataMenu();
+  }
+
+  Future loadDataMenu() async {
+    setState(() {
+      loading = true;
+    });
+    var jsonText = await rootBundle.loadString('assets/data/menu.json');
+    var result = await json.decode(jsonText);
+    // print(result["menus"]);
+    setState(() {
+      menu = result["menus"];
+      loading = false;
+    });
+  }
+
+  void searchData(text) {
+    text = text.toLowerCase();
+    var resultMenu = menu.where((element) {
+      var menuTitle = element["menu_name"].toLowerCase();
+      return menuTitle.contains(text);
+    }).toList();
+    // setState(() {
+    //   indexSearch = text;
+    // });
+
+    print(resultMenu);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +71,9 @@ class _SearchScreenState extends State<SearchScreen> {
           height: 35,
           child: TextField(
             autofocus: true,
+            onChanged: (text) {
+              searchData(text);
+            },
             decoration: InputDecoration(
               filled: true,
               fillColor: Colors.white,
