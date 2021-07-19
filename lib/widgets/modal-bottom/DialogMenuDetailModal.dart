96b88724dcc5295b5b7644509cf38828f1a0ac49
aisy <aisymuhammad@gmail.com>
2021-07-19 10:38:30 +0700

    change layout from container to scaffold for fixing keyboard (tested in Android not tested in web)

--- lib/widgets/modal-bottom/DialogMenuDetailModal.dart
+++ lib/widgets/modal-bottom/DialogMenuDetailModal.dart
@@ -99,172 +99,175 @@ class _DialogMenuDetailModalState extends State<DialogMenuDetailModal> {
       minChildSize: 0.5,
       maxChildSize: 0.8,
       builder: (_, controller) {
-        return Container(
-          padding: EdgeInsets.only(
-            right: 20.0,
-            left: 20.0,
-            bottom: 20.0,
-            top: 10.0,
-          ),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(20),
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 0.0,
+            toolbarHeight: 30,
+            backgroundColor: Colors.white,
+            automaticallyImplyLeading: false,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(20),
+              ),
+            ),
+            centerTitle: true,
+            title: Container(
+              child: null,
+              height: 4,
+              width: 30,
+              // margin: EdgeInsets.only(bottom: 20.0),
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(30),
+              ),
             ),
           ),
-          child: Column(
-            children: <Widget>[
-              Center(
-                child: Container(
-                  child: null,
-                  height: 4,
-                  width: 30,
-                  margin: EdgeInsets.only(bottom: 20.0),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(30),
+          backgroundColor: Colors.transparent,
+          body: Container(
+            height: double.infinity,
+            color: Colors.white,
+            padding: EdgeInsets.only(
+              right: 20.0,
+              left: 20.0,
+              bottom: 20.0,
+              top: 10.0,
+            ),
+            child: SingleChildScrollView(
+              controller: controller,
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20.0),
+                    height: 200,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(widget.dataMenu["menu_img"]),
+                          fit: BoxFit.cover),
+                    ),
+                    child: null,
                   ),
-                ),
-              ),
-              Flexible(
-                child: ListView(
-                  controller: controller,
-                  children: [
-                    // Menu Image
-                    Container(
-                      margin: EdgeInsets.only(bottom: 20.0),
-                      height: 200,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: NetworkImage(widget.dataMenu["menu_img"]),
-                            fit: BoxFit.cover),
-                      ),
-                      child: null,
+                  // Menu Name
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      widget.dataMenu["menu_name"],
+                      style:
+                          TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
                     ),
-                    // Menu Name
-                    Container(
-                      margin: EdgeInsets.only(bottom: 20),
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        widget.dataMenu["menu_name"],
-                        style: TextStyle(
-                            fontWeight: FontWeight.w700, fontSize: 20),
-                      ),
-                    ),
-                    // Menu Desc
-                    Container(
-                      child: Text(widget.dataMenu["description"]),
-                    ),
-                    // Check Option
-                    widget.dataMenu["price"].length > 1
-                        ? Container(
-                            margin: EdgeInsets.only(top: 20, bottom: 5),
-                            child: Text(
-                              "Pilihan",
-                              style: TextStyle(fontWeight: FontWeight.w700),
-                            ),
-                          )
-                        : SizedBox(),
-                    widget.dataMenu["price"].length > 1
-                        ? OptionPriceWidget(
-                            dataOption: widget.dataMenu["price"],
-                            realValue: (int newValue, String newName) {
-                              updatePrice(newValue, newName);
-                            },
-                          )
-                        : SizedBox(
-                            height: 20,
+                  ),
+                  // Menu Desc
+                  Container(
+                    child: Text(widget.dataMenu["description"]),
+                  ),
+                  // Check Option
+                  widget.dataMenu["price"].length > 1
+                      ? Container(
+                          alignment: Alignment.topLeft,
+                          margin: EdgeInsets.only(top: 20, bottom: 5),
+                          child: Text(
+                            "Pilihan",
+                            style: TextStyle(fontWeight: FontWeight.w700),
                           ),
-                    Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          InkWell(
-                            child: Container(
-                              width: 40,
-                              height: 40,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  shape: BoxShape.rectangle,
-                                  border: Border.all(
-                                    width: 2,
-                                    color: Colors.green,
-                                  )),
-                              child: Icon(Icons.remove, color: Colors.green),
-                            ),
-                            onTap: () {
-                              print(qtyItem);
-                              if (qtyItem <= 1) {
-                                setState(() {
-                                  qtyItem = 1;
-                                  qty.text = qtyItem.toString();
-                                });
-                              } else {
-                                setState(() {
-                                  qtyItem = qtyItem - 1;
-                                  qty.text = qtyItem.toString();
-                                  fixPrice = priceValue * qtyItem;
-                                });
-                              }
-                            },
+                        )
+                      : SizedBox(),
+                  widget.dataMenu["price"].length > 1
+                      ? OptionPriceWidget(
+                          dataOption: widget.dataMenu["price"],
+                          realValue: (int newValue, String newName) {
+                            updatePrice(newValue, newName);
+                          },
+                        )
+                      : SizedBox(
+                          height: 20,
+                        ),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        InkWell(
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                shape: BoxShape.rectangle,
+                                border: Border.all(
+                                  width: 2,
+                                  color: Colors.green,
+                                )),
+                            child: Icon(Icons.remove, color: Colors.green),
                           ),
-                          Container(
-                              margin: EdgeInsets.symmetric(horizontal: 10),
-                              width: 40,
-                              child: TextField(
-                                decoration: InputDecoration(
-                                  contentPadding: EdgeInsets.all(10.0),
-                                ),
-                                controller: qty,
-                                onChanged: (text) {
-                                  changeQtyValue(qty.text);
-                                },
-                              )),
-                          InkWell(
-                            child: Container(
-                              width: 40,
-                              height: 40,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  shape: BoxShape.rectangle,
-                                  border: Border.all(
-                                    width: 2,
-                                    color: Colors.green,
-                                  )),
-                              child: Icon(Icons.add, color: Colors.green),
-                            ),
-                            onTap: () {
-                              print(qtyItem);
+                          onTap: () {
+                            if (qtyItem <= 1) {
                               setState(() {
-                                qtyItem = qtyItem + 1;
+                                qtyItem = 1;
+                                qty.text = qtyItem.toString();
+                              });
+                            } else {
+                              setState(() {
+                                qtyItem = qtyItem - 1;
                                 qty.text = qtyItem.toString();
                                 fixPrice = priceValue * qtyItem;
                               });
-                            },
+                            }
+                          },
+                        ),
+                        Container(
+                            margin: EdgeInsets.symmetric(horizontal: 10),
+                            width: 40,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.all(10.0),
+                              ),
+                              controller: qty,
+                              onChanged: (text) {
+                                changeQtyValue(qty.text);
+                              },
+                            )),
+                        InkWell(
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                shape: BoxShape.rectangle,
+                                border: Border.all(
+                                  width: 2,
+                                  color: Colors.green,
+                                )),
+                            child: Icon(Icons.add, color: Colors.green),
                           ),
-                        ],
+                          onTap: () {
+                            setState(() {
+                              qtyItem = qtyItem + 1;
+                              qty.text = qtyItem.toString();
+                              fixPrice = priceValue * qtyItem;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    color: Colors.red,
+                    margin: EdgeInsets.only(top: 20),
+                    child: ElevatedButton(
+                      onPressed: () => {setOrder()},
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all(EdgeInsets.all(20)),
+                      ),
+                      child: Text(
+                        "Tambahkan ke pesanan - ${idrCurrencyFormat.format(fixPrice == 0 ? priceValue : fixPrice)}",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
                     ),
-                    Container(
-                      margin: EdgeInsets.only(top: 20),
-                      child: ElevatedButton(
-                        onPressed: () => {setOrder()},
-                        style: ButtonStyle(
-                          padding:
-                              MaterialStateProperty.all(EdgeInsets.all(20)),
-                        ),
-                        child: Text(
-                          "Tambahkan ke pesanan - ${idrCurrencyFormat.format(fixPrice == 0 ? priceValue : fixPrice)}",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         );
       },
