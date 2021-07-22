b0f89ded58e3ad95aa22d9c0caf737ace4413db3
aisy <aisymuhammad@gmail.com>
2021-07-22 09:09:41 +0700

    fix bug if you add same item and fix add item with different option

--- lib/models/orderModel.dart
+++ lib/models/orderModel.dart
@@ -2,37 +2,37 @@ import 'package:flutter/cupertino.dart';
 
 class OrderModel with ChangeNotifier {
   // format model dummy for testing
-  List orderValue = [
-    {
-      "id": 2,
-      "menu_name": "Sirloin",
-      "menu_img": "https://i.ibb.co/89p5fdR/abuba-Sirloin.jpg",
-      "qty": 1,
-      "option": "Wagyu MB 6-7",
-      "price": 225000,
-      "qty_price": 225000
-    },
-    {
-      "id": 3,
-      "menu_name": "Rib Eye",
-      "menu_img": "https://i.ibb.co/6wDPLM3/abuba-Rib-Eye.jpg",
-      "qty": 1,
-      "option": "United State rib eye",
-      "price": 135000,
-      "qty_price": 135000
-    },
-    {
-      "id": 13,
-      "menu_name": "Spaghetty Bolognese",
-      "menu_img": "https://i.ibb.co/syJRyYL/abuba-Spaghetti-Bolognese.png",
-      "qty": 1,
-      "option": "",
-      "price": 51000,
-      "qty_price": 51000
-    },
-  ];
+  // List orderValue = [
+  //   {
+  //     "id": 2,
+  //     "menu_name": "Sirloin",
+  //     "menu_img": "https://i.ibb.co/89p5fdR/abuba-Sirloin.jpg",
+  //     "qty": 1,
+  //     "option": "Wagyu MB 6-7",
+  //     "price": 225000,
+  //     "qty_price": 225000
+  //   },
+  //   {
+  //     "id": 3,
+  //     "menu_name": "Rib Eye",
+  //     "menu_img": "https://i.ibb.co/6wDPLM3/abuba-Rib-Eye.jpg",
+  //     "qty": 1,
+  //     "option": "United State rib eye",
+  //     "price": 135000,
+  //     "qty_price": 135000
+  //   },
+  //   {
+  //     "id": 13,
+  //     "menu_name": "Spaghetty Bolognese",
+  //     "menu_img": "https://i.ibb.co/syJRyYL/abuba-Spaghetti-Bolognese.png",
+  //     "qty": 1,
+  //     "option": "",
+  //     "price": 51000,
+  //     "qty_price": 51000
+  //   },
+  // ];
 
-  // List orderValue = [];
+  List orderValue = [];
 
   int get countItem => orderValue.length;
 
@@ -44,23 +44,20 @@ class OrderModel with ChangeNotifier {
     var index =
         orderValue.indexWhere((element) => element["id"] == value["id"]);
     var checkId = orderValue.where((element) => element["id"] == value["id"]);
+    var checkOption =
+        orderValue.where((element) => element["option"] == value["option"]);
 
     if (checkId.isEmpty) {
       orderValue.add(value);
     } else {
-      var qty = value["qty"] + value["qty"];
+      if (checkOption.isEmpty) {
+        orderValue.add(value);
+      } else {
+        var qty = value["qty"] + value["qty"];
 
-      orderValue[index]["qty"] = qty;
-      orderValue[index]["price"] = value["price"] * qty;
-      // orderValue[index] = {
-      //   "id": value["id"],
-      //   "menu_name": value["menu_name"],
-      //   "menu_img": "https://i.ibb.co/89p5fdR/abuba-Sirloin.jpg",
-      //   "qty": qty,
-      //   "option": "United State sirloin",
-      //   "price": value["price"] * qty
-      // };
-      // orderValue.remove(value);
+        orderValue[index]["qty"] = qty;
+        orderValue[index]["qty_price"] = value["price"] * qty;
+      }
     }
 
     notifyListeners();
@@ -69,13 +66,10 @@ class OrderModel with ChangeNotifier {
   void incrementItem(id) {
     var index = orderValue.indexWhere((element) => element["id"] == id);
     var qty = 1;
-    var defaultPrice = orderValue[index]["price"];
 
     orderValue[index]["qty"] = orderValue[index]["qty"] + qty;
     orderValue[index]["qty_price"] =
         orderValue[index]["price"] * orderValue[index]["qty"];
-
-    print(defaultPrice);
 
     notifyListeners();
   }
@@ -91,8 +85,6 @@ class OrderModel with ChangeNotifier {
       orderValue[index]["qty_price"] =
           orderValue[index]["qty_price"] - orderValue[index]["price"];
     }
-
-    print(orderValue[index]["qty_price"]);
 
     notifyListeners();
   }
